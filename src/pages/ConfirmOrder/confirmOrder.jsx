81dc2c37eac6108373badfabf0473c823061409c
FILIPIMDELAS <filipecristovam2020@gmail.com>
2025-02-06 14:41:44 -0300

    new apresentation confirmOrder

--- src/pages/ConfirmOrder/confirmOrder.jsx
+++ src/pages/ConfirmOrder/confirmOrder.jsx
@@ -2,8 +2,6 @@ import style from './confirmOrder.module.css'
 import Layout from '../../components/layout/layout'
 import { useGlobalContext } from '../../hooks/globalContext'
 import { useState, useEffect } from 'react'
-import IconLabel from '../../components/iconLabel/iconLabel'
-import Buttons from '../../components/buttons/buttons'
 import MapsKart from '../../components/mapsKart/mapsKart'
 
 export default function ConfirmOrder() {
@@ -15,19 +13,25 @@ export default function ConfirmOrder() {
     setSubTotal(cardItens.reduce((acc, produto) => acc + parseFloat(produto.price), 0))
   }, [cardItens])
 
+  const handleCep = (e) => {
+    setCep(e.target.value)
+  }
+
   return (
     <Layout>
-      <div className={style.div_contentConfirmerOrder}>
-        <div className={style.div_contentMapsConfirmerOrder}>
-          <MapsKart />
+      <div className={style.div_contentConfirmOrder}>
+        <div className={style.div_contentConfirmerOrder}>
+          <div className={style.div_contentMapsConfirmerOrder}>
+            <MapsKart />
+          </div>
+            <div className={style.div_ConsultCEPConfirmerOrder}>.
+            <input type="number" placeholder='CEP' onChange={handleCep} value={cep}/>
+          </div>
         </div>
         <div className={style.div_contentResumeConfirmerOrder}>
           <p>SubTotal: R$ {subtotal}</p>
           <p>Valor do CEP: R$ {cep}</p>
         </div>
-      </div>
-      <div className={style.div_ConsultCEPConfirmerOrder}>.
-        <input type="number" placeholder='CEP'/>
       </div>
     </Layout>
   )
